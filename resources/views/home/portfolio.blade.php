d6a127570a0dbf1b191840207689453ac189b7dd
Reza <wardhana15.aw@gmail.com>
2024-06-02 08:29:53 +0700

    menambahkan data kursi dan mengganti kategori

--- resources/views/home/portfolio.blade.php
+++ resources/views/home/portfolio.blade.php
@@ -84,9 +84,9 @@
                 <div class="col-md-12 d-flex justify-content-center">
                     <ul class="list-unstyled d-flex portfolio-filters">
                         <li data-filter="*" class="py-2 px-4 filter-active text-white">ALL</li>
-                        <li data-filter=".filter-web" class="py-2 px-4">Web</li>
-                        <li data-filter=".filter-design" class="py-2 px-4">Design</li>
-                        <li data-filter=".filter-photo" class="py-2 px-4">Photography</li>
+                        <li data-filter=".filter-chair" class="py-2 px-4">Chair</li>
+                        <li data-filter=".filter-table" class="py-2 px-4">Table</li>
+                        <li data-filter=".filter-other" class="py-2 px-4">Other</li>
                     </ul>
                 </div>
             </div>
@@ -94,7 +94,7 @@
             <div class="row mt-5">
                 <div class="col-md-12">
                     <div class="mansory portfolio-container">
-                        <div class="mansory-sizer"></div>
+                        <div class="mansory-sizer "></div>
                         @foreach ($portfolio as $portfolio)
                         <div class="mansory-item m-2 portfolio-item filter-web">
                         <img src="image/{{$portfolio->image}}" alt="" class="img-fluid" />
